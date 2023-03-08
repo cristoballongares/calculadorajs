e4a87e98f8d4ea9eab14158cd21cda48bdc4fa5e
crist138 <cristoballongares@gmail.com>
2023-03-07 20:28:46 -0600

    first
    first

---
+++ app.js
@@ -0,0 +1,10 @@
+function seleccionarNum(numero) {
+    var res = document.getElementById("resultado");
+
+    var valorA = resultado.value;
+
+    var valorN = valorA + numero;
+
+    resultado.value = valorN;
+
+}
