b795cb83aa5e3ea1bba189bb1223c8fdb7d76d13
crist138 <cristoballongares@gmail.com>
2023-03-19 13:44:13 -0600

    lolo
    lol

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var turno = true;
+
 function seleccionarNum(numero) {
     var res = document.getElementById("resultado");
 
@@ -7,4 +9,39 @@ function seleccionarNum(numero) {
 
     resultado.value = valorN;
 
+}
+
+function borrar(){
+    var res = document.getElementById("resultado");
+    res.value = "";
+}
+
+function sumar(){
+    var btnSum = document.getElementById("suma")
+    var resultado = document.getElementById("resultado");
+    var igual = document.getElementById("igual");
+
+    if(turno){
+        n1 = parseInt(resultado.value);
+        
+        console.log(n1,turno);
+        turno = false;    
+    } else if(!turno){
+        n2 = parseInt(resultado.value);
+
+        n1+n2;
+        console.log(n1+n2,turno);
+        igual.onclick(getRes(1,n1,n2));
+        turno = true;
+    }
+}
+
+
+function getRes(tipo,n1,n2){
+var resultado = document.getElementById("resultado");
+
+    switch(tipo){
+        case 1: resultado.value = parseInt(n1+n2); break; // suma
+        case 2: return n1-n2; break; // resta
+        }
 }
